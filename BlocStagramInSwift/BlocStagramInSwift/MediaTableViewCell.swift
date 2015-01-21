d0cfe56f1fd14eaa85a0fe68c6633ed508c6387b
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2015-01-20 18:32:30 -0700

    implemented table view controller

---
+++ BlocStagramInSwift/BlocStagramInSwift/MediaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MediaTableViewCell.swift
+//  BlocStagramInSwift
+//
+//  Created by Sameer Totey on 1/20/15.
+//  Copyright (c) 2015 Sameer Totey. All rights reserved.
+//
+
+import UIKit
+
+class MediaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mediaCellImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BlocStagramInSwift/BlocStagramInSwift/MediaTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ImagesTableViewController.swift
+//  BlocStagramInSwift
+//
+//  Created by Sameer Totey on 1/20/15.
+//  Copyright (c) 2015 Sameer Totey. All rights reserved.
+//
+
+import UIKit
+
+let cellReuseIdentifier = "mediaCell"
+
+class MediaTableViewController: UITableViewController {
+    var images = [UIImage]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        for index in 0...9 {
+            let image = UIImage(named: "\(index).jpg")
+            if let newImage = image {
+                images.append(newImage)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return images.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) as MediaTableViewCell
+
+        // Configure the cell...
+        cell.mediaCellImageView.image = images[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView,
+        heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+            let image = images[indexPath.row]
+            return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
